c7fc3116014d1ce94b886d75798093e2fd86bda6
Leonardo de Moura <121776058+leonardodemouragomes@users.noreply.github.com>
2023-01-17 22:48:44 -0300

    aulas de laços

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c += 1
+} while (c <= 6)
+
+/*
+var c = 1
+while (c < 500) {
+    console.log('Tudo bem?')
+    c += 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
